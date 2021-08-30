d020b838e9debc8f98c4fda2d20b974b7bc2ccb1
Ali Erdem Akın <erdem.akin@aerotek.com.tr>
2021-08-31 02:11:42 +0300

    Fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,11 +19,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
             'database' => ':memory:',
             'prefix'   => '',
         ]);
-
-        include_once __DIR__ . '/../database/migrations/create_users_table.php.stub';
-
-        // run the migration's up() method
-        (new \CreateUsersTable)->up();
     }
 
     protected function getPackageProviders($app)
