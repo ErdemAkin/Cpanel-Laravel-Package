84e6e778f1bd451a2d0e8aa0f2bd18bd9398d174
Ali Erdem Akın <erdem.akin@aerotek.com.tr>
2021-09-02 18:02:20 +0300

    Fix

--- src/Http/Controllers/FtpController.php
+++ src/Http/Controllers/FtpController.php
@@ -3,6 +3,7 @@
 namespace Laravel\Cpanel\Http\Controllers;
 
 use Cpanel\Client;
+use Cpanel\Exception\CpanelErrorException;
 use Cpanel\Request\Uapi\Ftp;
 
 class FtpController extends Controller
@@ -10,17 +11,29 @@ class FtpController extends Controller
     protected $cpanelApiClient;
     protected $ftpClient;
 
-    public function __construct(){
-        $this->cpanelApiClient = new Client(config("cpanel.domain"));
-        $this->cpanelApiClient->authenticateByUsernamePassword(config("cpanel.username"), config("cpanel.password"));
+    public function __construct()
+    {
+        try {
+            $this->cpanelApiClient = new Client(config("cpanel.domain"));
+            $this->cpanelApiClient->authenticateByUsernamePassword(config("cpanel.username"), config("cpanel.password"));
 
-        $this->ftpClient = new Ftp($this->cpanelApiClient);
+            $this->ftpClient = new Ftp($this->cpanelApiClient);
+        } catch (CpanelErrorException $ex) {
+            abort(500, $ex->getMessage());
+        }
     }
+
     public function index()
     {
-        return view('cpanel::ftp.index', [
-            'ftpList' => $this->ftpClient->getFtpAccounts()
-        ]);
+        try {
+            $ftpList = $this->ftpClient->getFtpAccounts();
+
+            return view('cpanel::ftp.index', [
+                'ftpList' => $ftpList['data'],
+            ]);
+        } catch (CpanelErrorException $ex) {
+            abort(500, $ex->getMessage());
+        }
     }
 
     public function store()
